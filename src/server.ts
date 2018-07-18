a49b4939d496ddf92c30a8ab4e0db57e8c2d4980
Fedor Indutny <fedor@indutny.com>
2018-07-18 13:49:50 -0400

    save

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,34 @@
 import * as ws from 'ws';
 import * as http from 'http';
 
+import { Generator } from './generator';
 import { Player } from './player';
 import { Game } from './game';
 
 export interface IServerOptions {
   readonly timeout?: number;
   readonly parallelGames?: number;
+
+  readonly types?: number;
+  readonly minObj?: number;
+  readonly maxObj?: number;
+  readonly total?: number;
+  readonly maxRounds?: number;
 }
 
 interface IDefiniteServerOptions {
   readonly timeout: number;
   readonly parallelGames: number;
+
+  readonly types: number;
+  readonly minObj: number;
+  readonly maxObj: number;
+  readonly total: number;
+  readonly maxRounds: number;
 }
 
 export class Server extends http.Server {
+  private readonly generator: Generator;
   private readonly ws: ws.Server = new ws.Server({ server: this });
   private readonly pool: Set<Player> = new Set();
   private readonly options: IDefiniteServerOptions;
@@ -26,7 +40,17 @@ export class Server extends http.Server {
     this.options = Object.assign({
       timeout: 2000,
       parallelGames: 1000,
+
+      types: 3,
+      minObj: 1,
+      maxObj: 6,
+      total: 10,
+      maxRounds: 5,
     }, options);
+
+    // TODO(indutny): just pass the object
+    this.generator = new Generator(this.options.types, this.options.minObj,
+      this.options.maxObj, this.options.total, this.options.maxRounds);
 
     this.ws.on('connection', (socket) => this.onConnection(socket));
 
