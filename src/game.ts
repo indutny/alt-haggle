f93abbfba0bd95562dade9733b57d6d4901cc55c
Fedor Indutny <fedor@indutny.com>
2018-07-18 14:00:09 -0400

    save

--- src/game.ts
+++ src/game.ts
@@ -28,10 +28,73 @@ export class Game {
   }
 
   public async run(): Promise<IGameResult> {
+    // Send configuration to both players
+    await Promise.all([
+      () => {
+        return this.first.start(this.id, {
+          counts: this.config.counts,
+          values: this.config.valuations.first,
+          maxRounds: this.config.maxRounds
+        });
+      },
+      () => {
+        return this.second.start(this.id, {
+          counts: this.config.counts,
+          values: this.config.valuations.first,
+          maxRounds: this.config.maxRounds
+        });
+      },
+    ]);
+
+    let offer: Offer | undefined = undefined;
+    for (let i = 0; i < this.config.maxRounds; i++) {
+      let counterOffer = await this.first.step(this.id, offer);
+      if (counterOffer === undefined) {
+        if (i === 0) {
+          throw new Error('Invalid first offer');
+        }
+
+        return this.result(offer!, this.invertOffer(offer!));
+      }
+      offer = this.invertOffer(counterOffer);
+
+      counterOffer = await this.second.step(this.id, offer);
+
+      if (counterOffer === undefined) {
+        return this.result(this.invertOffer(offer!), offer!);
+      }
+      offer = this.invertOffer(counterOffer);
+    }
+
+    // Round limit
     return {
       accept: false,
       first: 0,
       second: 0,
+    };
+  }
+
+  private invertOffer(offer: Offer): Offer {
+    const res = Array.from(offer);
+    for (let i = 0; i < res.length; i++) {
+      res[i] = this.config.counts[i] - res[i];
+    }
+    return res;
+  }
+
+  private offerValue(offer: Offer, values: Values): number {
+    let res = 0;
+    for (let i = 0; i < offer.length; i++) {
+      res += offer[i] * values[i];
+    }
+    return res;
+  }
+
+  private result(first: Offer, second: Offer): IGameResult {
+    return {
+      accept: true,
+      first: this.offerValue(first, this.config.valuations.first),
+      second: this.offerValue(second, this.config.valuations.second),
     };
   }
 }
